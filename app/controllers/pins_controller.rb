3371ec3ccd6923fd43f9b838cdf22efdd728453c
Ryan Carter <cartervisuals@gmail.com>
2015-07-28 10:56:20 -0600

    added save in controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -21,7 +21,7 @@ class PinsController < ApplicationController
 
   def create
     @pin = current_user.pins.build(pin_params)
-
+    @pin.save
 
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
