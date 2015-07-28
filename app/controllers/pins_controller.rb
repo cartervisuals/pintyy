96ab70f5f34c815770eafe687de704de3770e49f
Ryan Carter <cartervisuals@gmail.com>
2015-07-28 09:45:43 -0600

    gen update

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,59 @@
+class PinsController < ApplicationController
+
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
+  def index
+    @pins = Pin.all
+  end
+
+  def show
+    @pin = Pin.find(params[:id])
+  end
+
+  def new
+    @pin = current_user.pins.build
+  end
+
+  def edit
+    @pin = Pin.find(params[:id])
+  end
+
+  def create
+    @pin = current_user.pins.build(pin_params)
+
+
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render :new  
+    end
+  end
+
+  def update
+    @pin = Pin.find(params[:id])
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @pin = Pin.find(params[:id])
+    @pin.destroy
+    redirect_to pins_url, notice: 'Pin was successfully deleted.'
+  end
+
+  private
+
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+    end
+
+    def pin_params
+      params.require(:pin).permit(:description)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150728020036) do
+ActiveRecord::Schema.define(version: 20150728034557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pins", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
+
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
